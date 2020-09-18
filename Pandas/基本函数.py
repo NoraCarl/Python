d1d2c82cbbcb93def48e4ce83f8573258500e65e
Cora <17623461751@163.com>
2020-09-18 10:19:52 +0800

    2

--- Pandas/基本函数.py
+++ Pandas/基本函数.py
@@ -6,3 +6,5 @@ Data_csv.rename(columns={'School':'学校','Class':'班级','ID':'学生ID','Gen
                         'Height':'身高','Weight':'体重','Math':'数学成绩','Physics':'物理成绩'},inplace=True)
 print(Data_csv.head())  #查看头数据
 print(Data_csv.tail())  #查看尾数据
+print(Data_csv['物理成绩'].nunique()) # nunique显示该字段里面有多少唯一值
+print(Data_csv['物理成绩'].unique()) #unique显示该字段所有的唯一值
