ba0a3c1ed27657bee7d8c470d6e8bfa767bdad49
Cora <17623461751@163.com>
2020-09-18 10:15:04 +0800

    1

---
+++ Pandas/DataFrame_i.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({'编号':list('12345'),'分值':range(1,6)},index=list(1,2,3,4,5))
---
+++ Pandas/基本函数.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+Data_csv = pd.read_csv('C:\\Users\\cora\\Desktop\\table.csv')
+Data_csv.rename(columns={'School':'学校','Class':'班级','ID':'学生ID','Gender':'性别','Address':'地址',
+                        'Height':'身高','Weight':'体重','Math':'数学成绩','Physics':'物理成绩'},inplace=True)
+print(Data_csv.head())  #查看头数据
+print(Data_csv.tail())  #查看尾数据
