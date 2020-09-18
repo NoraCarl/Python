44a88e7578035c98135962bddaaa4f91dd563a38
Cora <17623461751@163.com>
2020-09-18 15:44:59 +0800

    replace

--- Pandas/重复值和缺失值.py
+++ Pandas/重复值和缺失值.py
@@ -14,5 +14,9 @@ print(Data_csv.sort_values(by='Math',ascending=False).head(3))
 print(Data_csv.sort_values(by=['Math','Height'],ascending=False))
 '''
 # 重复值
-# duplicated 告诉我们是否重复以Boolean返回
+# duplicated 告诉我们是否重复以Boolean返回 第一次出现为False 之后重复可多次出现True
 print(Data_csv.duplicated('Address'))
+# drop_duplicates
+# last 保留重复值中最后出现的 first保留重复值中第一次出现的
+print(Data_csv.drop_duplicates('Class',keep='last')) # 保留删除重复值的唯一项
+print(Data_csv.drop_duplicates(['School','Class'],keep = 'last'))  # 两则相同 则重复
