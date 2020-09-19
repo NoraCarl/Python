c6200cc210e27dafb2042c7f8a9f14e53a963e54
Cora <17623461751@163.com>
2020-09-19 16:39:03 +0800

    1

--- 可视化/散点图.py
+++ 可视化/散点图.py
@@ -5,3 +5,18 @@ import matplotlib.pyplot as plt
 #设置中文
 plt.rcParams['font.sans-serif']=['Simhei']
 plt.rcParams['axes.unicode_minus']=False
+
+# 设置随机值
+N = 10000
+x = np.random.randn(N)
+y = np.random.randn(N)
+
+
+if __name__ == "__main__":
+    plt.figure(figsize=(12,6))
+    plt.scatter(x, y,s=100,alpha=0.5,edgecolors= 'white')
+    plt.title('complex',fontsize=30)#显示图表标题
+    plt.xlabel('这里是x轴')#x轴名称
+    plt.ylabel('这里是y轴')#y轴名称
+    plt.grid(False)#显示网格线
+    plt.show()
---
+++ 可视化/矩阵图.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# 设置中文
+plt.rcParams['font.sans-serif']=['Simhei']
+plt.rcParams['axes.unicode_minus']=False
